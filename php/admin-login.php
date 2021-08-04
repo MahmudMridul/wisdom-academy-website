6525bb3552b22c9deb57bd2c69602cbe1fdec422
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2021-08-04 15:40:28 +0600

    created login system for admin

--- php/admin-login.php
+++ php/admin-login.php
@@ -17,21 +17,21 @@
 
       <h1>Login</h1>
 
-      <form class="box" action="index.html" method="post">
+      <form class="box" action="" method="post">
 
         <div class="txt_field">
-          <input type="text" required>
+          <input type="text" name="username" required>
           <span></span>
           <label>Username</label>
         </div>
 
         <div class="txt_field">
-          <input type="password" required>
+          <input type="password" name="password" required>
           <span></span>
           <label>Password</label>
         </div>
 
-        <input type="submit" value="Login">
+        <input type="submit" name="submit" value="Login">
 
       </form>
 
@@ -40,6 +40,31 @@
     <a href="../index.html">
       <img src="../image/icon/exit.png" alt="">
     </a>
+
+    <?php
+    error_reporting(E_PARSE | E_ERROR);
+    require("connect.php");
+
+    $username = $_REQUEST["username"];
+    $password = $_REQUEST["password"];
+
+    $query = "SELECT * FROM admin WHERE username = '{$username}' AND password = '{$password}'";
+    $result = mysqli_query($connection, $query);
+
+    if($_SERVER["REQUEST_METHOD"] == "POST")
+    {
+      if(mysqli_num_rows($result) > 0)
+      {
+        header("Location: admin-page.php");
+      }
+      else
+      {
+        echo "<script> alert('Wrong username or password'); window.location = 'admin-login.php'; </script>";
+      }
+    }
+
+
+    ?>
 
   </body>
 </html>
