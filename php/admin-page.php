e3d7d8f52958607472646caa05c1297f77956dce
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2021-08-05 23:44:27 +0600

    news add delete feature added

--- php/admin-page.php
+++ php/admin-page.php
@@ -171,7 +171,6 @@
 
 
       <?php
-        mysqli_report(MYSQLI_REPORT_STRICT);
         require("connect.php");
 
         $query = "SELECT name, id FROM news";
@@ -207,6 +206,14 @@
 
           $query = "SELECT * FROM news WHERE id = {$news_id}";
           $result = mysqli_query($connection, $query);
+          $row = mysqli_fetch_assoc($result);
+          $name = $row["name"];
+          $extension = $row["extension"];
+          $directory = "../image/news/";
+          $filename = $name . "." . $extension;
+
+          chdir($directory);
+          unlink($filename);
 
           if(mysqli_num_rows($result) == 1)
           {
@@ -222,16 +229,117 @@
         mysqli_close($connection);
       ?>
 
+      <h2>Upload News Contents</h2>
+
+      <form class="" action="" method="post" enctype="multipart/form-data">
+
+        <label> Image File: </label>
+        <input type="file" name="news_upload[]" value="" multiple required><br>
+        <input type="submit" name="news_upload_button" value="Upload">
+
+      </form>
+
+      <?php
+        require("connect.php");
+
+        if($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['news_upload_button']))
+        {
+          if(isset($_FILES['news_upload']))
+          {
+            $file_array = reArrayFiles($_FILES['news_upload']);
+
+            for($i = 0; $i < count($file_array); ++$i)
+            {
+              if($file_array[$i]['error'])
+              {
+                echo "<script> alert('Something went wrong while uploading!'); window.location = 'admin-page.php'; </script>";
+              }
+              else
+              {
+                $extensions = array('jpg', 'jpeg', 'png');
+                $file_extension = explode('.', $file_array[$i]['name']);
+                $name = $file_extension[0];
+                $file_extension = end($file_extension);
+
+                if(!in_array($file_extension, $extensions))
+                {
+                  echo "<script> alert('Invalid file extension!'); window.location = 'admin-page.php'; </script>";
+                }
+                else
+                {
+                  $directory = "../image/news/" . $file_array[$i]['name'];
+                  move_uploaded_file($file_array[$i]['tmp_name'], $directory);
+
+                  $query = "INSERT INTO news(name, directory, extension) VALUES ('$name','$directory', '$file_extension')";
+                  $result = mysqli_query($connection, $query);
+                  echo "<script> alert('File uploaded successfully!'); window.location = 'admin-page.php'; </script>";
+                }
+              }
+            }
+          }
+        }
+
+        function reArrayFiles($file)
+        {
+          $file_ary = array();
+          $file_count = count($file['name']);
+          $file_key = array_keys($file);
+
+          for($i=0;$i<$file_count;$i++)
+          {
+            foreach($file_key as $val)
+            {
+                $file_ary[$i][$val] = $file[$val][$i];
+            }
+          }
+          return $file_ary;
+        }
+        mysqli_close($connection);
+      ?>
+
       </div>
 
       <div class="team" id="team">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-        cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-        est laborum.
+
+        <h2>Delete a Member</h2>
+
+        <form class="" action="" method="post">
+
+          <label> Select Member ID: </label>
+          <input type="number" name="member_id" value="" required><br>
+          <input type="submit" name="delete_member" value="Delete">
+
+        </form>
+
+        <h2>Team Members</h2>
+
+        <?php
+        require("connect.php");
+
+        $query = "SELECT * FROM team";
+        $result = mysqli_query($connection, $query);
+
+        if(mysqli_num_rows($result) >= 1)
+        {
+          echo "<table>";
+          echo "<thead>";
+          echo "<tr> <th> Name </th> <th> News ID </th>  </tr>";
+          echo "</thead>";
+          echo "<tbody>";
+          while($row = mysqli_fetch_assoc($result))
+          {
+              echo "<tr> <td>" . $row["name"] . "</td> <td> " . $row["id"] . "</td> </tr>";
+          }
+          echo "</tbody>";
+          echo "</table>";
+        }
+        else
+        {
+          echo "<h2> No news found </h2>";
+        }
+        mysqli_close($connection);
+      ?>
+
       </div>
 
       <div class="work" id="work">
